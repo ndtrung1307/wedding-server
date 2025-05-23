dbecb1e9e444c5cb9b73c48a70ee1b199d06f169
Trung Nguyen <trung.nguyen@planettota.com>
2025-05-23 14:13:44 +0700

    set cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "*", // Replace with your frontend URL
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: "Content-Type, Accept",
+  });
   await app.listen(3000);
 }
 bootstrap();
